bbaddb0adc95efd25cb2c5f6702ebc1e3e65cf70
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2023-04-13 23:09:24 +0300

    add HW3

---
+++ controllers/contacts-controllers.js
@@ -0,0 +1,66 @@
+const { Contact } = require("../models/contact");
+
+const { HttpError } = require("../helpers");
+
+const { ctrlWrapper } = require("../utils");
+
+const getAllContacts = async (req, res, next) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+const getContactById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res, next) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const deleteContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Delete success",
+  });
+};
+
+const updateContactById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+const updateFavoriteById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  updateContactById: ctrlWrapper(updateContactById),
+  updateFavoriteById: ctrlWrapper(updateFavoriteById),
+};
---
+++ models/contact.js
@@ -0,0 +1,62 @@
+const { Schema, model } = require("mongoose");
+
+const Joi = require("joi");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", (error, data, next) => {
+  error.status = 400;
+  next();
+});
+
+const addSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required name field` }),
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required email field` }),
+
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required phone field` }),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean()
+    .required()
+    .messages({ "any.required": "missing field favorite" }),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
